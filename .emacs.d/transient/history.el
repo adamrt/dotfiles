7733930bfa88fe3c2150443e15ff916e903fada1
Adam Patterson <adam@adamrt.com>
2020-05-17 23:19:13 -0500

    Add to gitignore and update tansient history

--- .emacs.d/transient/history.el
+++ .emacs.d/transient/history.el
@@ -1,15 +1,24 @@
-((magit-blame ("-w"))
+((magit-blame
+  ("-w"))
  (magit-branch nil)
- (magit-cherry-pick ("--ff"))
+ (magit-cherry-pick
+  ("--ff"))
  (magit-commit nil)
- (magit-diff ("--no-ext-diff" "--stat"))
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
  (magit-dispatch nil)
  (magit-fetch nil)
  (magit-gitignore nil)
- (magit-log ("-n256" "--graph" "--decorate") (("--" "simplex/sales_order_cmd_generator.go")) ("-n256" "--graph" "--color" "--decorate"))
- (magit-merge nil ("--ff-only"))
- (magit-pull nil)
- (magit-push nil ("--force-with-lease") ("--force"))
+ (magit-log
+  ("-n256" "--graph" "--decorate")
+  ("-n256" "--graph" "--color" "--decorate"))
+ (magit-merge nil
+              ("--ff-only"))
+ (magit-pull nil
+             ("--rebase"))
+ (magit-push nil
+             ("--force-with-lease")
+             ("--force"))
  (magit-rebase nil)
  (magit-reset nil)
  (magit-show-refs nil)
