6df7fca297d22fd2aef5be4b2a6c5256de485ee4
Adam Patterson <adam@adamrt.com>
2022-04-15 09:32:25 -0500

    Remove old files

--- bin/dock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-xrandr --output DP-1 --auto --primary
-xrandr --output eDP-1 --off
-# Unset ctrl:nocaps with empty options
-setxkbmap -option
--- bin/undock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xrandr --output eDP-1 --auto --primary
-xrandr --output DP-1 --off
-setxkbmap -option ctrl:nocaps
